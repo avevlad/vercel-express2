301a0015c715e79ae599da9e21154f352f5230e0
avevlad <vlad.open.source@gmail.com>
2021-01-06 01:30:39 +0300

    typescript

--- app/index.js
+++ app/index.js
@@ -1,24 +1,23 @@
 'use strict';
-
-const fastify = require('fastify');
-
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fastify_1 = __importDefault(require("fastify"));
 function build() {
-  const app = fastify({
-    logger: true
-  });
-
-  app.get('/', async (req, res) => {
-    const { name = 'World' } = req.query;
-    req.log.info({ name }, 'hello world!');
-    return `Hello ${name}!`;
-  });
-  app.get('/another/kek', async (req, res) => {
-    const { name = 'World' } = req.query;
-    req.log.info({ name }, 'hello world!');
-    return `Hello ${name}!`;
-  });
-
-  return app;
+    const app = fastify_1.default({
+        logger: true
+    });
+    app.get('/', async (req, res) => {
+        const { name = 'World' } = req.query;
+        // req.log.info({ name }, 'hello world!');
+        return `Hello ${name}!`;
+    });
+    app.get('/another/kek', async (req, res) => {
+        const { name = 'World' } = req.query;
+        // req.log.info({ name }, 'hello world!');
+        return `Hello ${name}!`;
+    });
+    return app;
 }
-
 module.exports = build;
---
+++ app/index.ts
@@ -0,0 +1,28 @@
+'use strict';
+
+import fastify from 'fastify';
+
+interface IQuerystring {
+  name: string;
+}
+
+function build() {
+  const app = fastify({
+    logger: true
+  });
+
+  app.get<{ Querystring: IQuerystring }>('/', async (req, res) => {
+    const { name = 'World' } = req.query;
+    // req.log.info({ name }, 'hello world!');
+    return `Hello ${name}!`;
+  });
+  app.get<{ Querystring: IQuerystring }>('/another/kek', async (req, res) => {
+    const { name = 'World' } = req.query;
+    // req.log.info({ name }, 'hello world!');
+    return `Hello ${name}!`;
+  });
+
+  return app;
+}
+
+module.exports = build;
